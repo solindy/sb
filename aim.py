7bdcf017d26e8722794aca7be913f85869875aac
solindy <69877749+solindy@users.noreply.github.com>
2020-08-22 16:57:43 +0900

    Update aim.py

--- aim.py
+++ aim.py
@@ -16,20 +16,23 @@ async def on_ready():
 async def on_message(message):
     if message.author.bot:
         return None
-    if message.content.startswith('!sb dm'):
-        for i in message.guild.members:
-            if i.bot == True:
-                pass
-            else:
-                try:
-                    msg = message.content[7:]
-                    embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="Various Games Server 공지")
-                    embed.add_field(name="-----------------------------------------", value=msg, inline=True)
-                    embed.set_footer(text=f"https://discord.gg/3wX5cHh")
-                    await i.send(embed=embed)
-                except:
-                    pass
-
+        if message.content.startswith('!sb dm'):
+            if message.author.guild_permissions.manage_messages:
+                for i in message.guild.members:
+                    if i.bot == True:
+                        pass
+                    else:
+                        try:
+                            msg = message.content[7:]
+                            embed = discord.Embed(colour=0x1DDB16, timestamp=message.created_at, title="Various Games Server 공지")
+                            embed.add_field(name="-----------------------------------------", value=msg, inline=True)
+                            embed.set_footer(text=f"https://discord.gg/3wX5cHh")
+                            await i.send(embed=embed)
+                        except:
+                            pass
+        else:
+            await message.channel.send("당신은 권한이 없기 때문에 전체DM 명령어를 사용하실 수 없습니다.")
+                    
     if message.content.startswith("토리 멍청이"):
         await message.channel.send("ㅇㅈ")
     if message.content.startswith("Tori is L"):
@@ -46,7 +49,7 @@ async def on_message(message):
         await message.channel.send("마트롤시카?!")
     if message.content.startswith("솔린디가 누구임"):
         await message.channel.send("나임")
-    if message.content.startswith("도토리묵")
+    if message.content.startswith("도토리묵"):
         await message.channel.send("도토리묵 맛있겠다")
 
     if message.content.startswith("!sb clean"):
@@ -67,8 +70,9 @@ async def on_message(message):
     if message.content.startswith("!sb"):
         mg = message.content[4:]
         if str(mg) == str("help"):
-            await message.channel.send("1. !sb dm <할 말> \n - 전체 DM 공지를 보냅니다\n2. !sb clean <숫자> \n - 정한 숫자만큼 밑에서부터 메세지를 삭제합니다.")
-
+            await message.channel.send("1. !sb dm <할 말> \n - 전체 DM 공지를 보냅니다\n2. !sb clean <숫자> \n - 정한 숫자만큼 밑에서부터 메세지를 삭제합니다 \n3. !sb msg \n - 사용 가능한 메시지 채팅 반응을 모두 표시합니다")
+        if str(mg == str("msg"):
+            await message.channel.send("토리 멍청이 / Tori is L / 반짝반짝 희벼리 / 마트롤시카 / 솔린디가 누구임 / 도토리묵")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
