1335bb46a8097676f358b8a2b8075db26a78b110
solindy <69877749+solindy@users.noreply.github.com>
2020-09-01 17:44:17 +0900

    Update aim.py

--- aim.py
+++ aim.py
@@ -82,8 +82,12 @@ async def on_message(message):
     if message.content.startswith("!sb"):
         mg = message.content[4:]
         if str(mg) == str("help"):
-            await message.channel.send(
-                "1. !sb dm <할 말> \n - 전체 DM 공지를 보냅니다\n2. !sb clean <숫자> \n - 정한 숫자만큼 밑에서부터 메세지를 삭제합니다 \n3. !sb msg \n - 사용 가능한 메시지 채팅 반응을 모두 표시합니다 \n4. !sb vote \n - 투표를 진행합니다")
+            embed = discord.Embed(title="Solindy Bot Help", description="솔린디 봇 도움말", color=0x00aaaa)
+            embed.add_field(name="1. !sb dm <할 말>", value=" - 전체 DM 공지를 보냅니다", inline=False)
+            embed.add_field(name="2. !sb clean <숫자>", value=" - 정한 숫자만큼 밑에서부터 메세지를 삭제합니다", inline=False)
+            embed.add_field(name="3. !sb msg", value=" - 사용 가능한 메시지 채팅 반응을 모두 표시합니다", inline=False)
+            embed.add_field(name="4. !sb vote <주제>", value=" - 주제에 대한 찬반 투표를 진행합니다", inline=False)
+            await message.channel.send(embed=embed)
         if str(mg) == str("msg"):
             await message.channel.send("토리 멍청이 / Tori is L / 반짝반짝 희벼리 / 마트롤시카 / 솔린디가 누구임 / 도토리묵")
 
